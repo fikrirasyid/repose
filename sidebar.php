d8db6e54b0507583ae50d18d44ea37137ae298bb
Fikri Rasyid <fikrirasyid@gmail.com>
2015-09-21 08:25:14 +0700

    Adding basic sidebar styling

--- sidebar.php
+++ sidebar.php
@@ -11,5 +11,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
+	<button class="sidebar-close" aria-controls="secondary" aria-expanded="true"><?php esc_html_e( 'Close', 'repose' ); ?></button>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
