b9df309f362cc0c0c4a1c88ed0a510aec415a57a
Fikri Rasyid <fikrirasyid@gmail.com>
2015-09-28 10:15:36 +0700

    Styling primary navigation's toggle menu

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 		<div class="wrap">
 			<h3 class="site-title-top"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h3>
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'repose' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'More', 'repose' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 			</nav><!-- #site-navigation -->
 		</div><!-- .wrap -->
