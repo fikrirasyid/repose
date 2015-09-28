f55e4ee43dc7a267456d63306f07aba8250b24e3
Fikri Rasyid <fikrirasyid@gmail.com>
2015-09-28 10:22:27 +0700

    Adding repose.js

--- functions.php
+++ functions.php
@@ -116,6 +116,8 @@ function repose_scripts() {
 
 	wp_enqueue_style( 'repose-style', get_stylesheet_uri(), array( 'repose-google-fonts' ) );
 
+	wp_enqueue_script( 'repose-script', get_template_directory_uri() . '/js/repose.js', array( 'jquery' ), '20150928', true );
+
 	wp_enqueue_script( 'repose-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'repose-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
