83d73f758c1088115ca5fd7d25a74a9971f452fe
Fikri Rasyid <fikrirasyid@gmail.com>
2015-09-28 10:44:19 +0700

    Adding activate secondary area mechanism

--- js/repose.js
+++ js/repose.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function($) {
+  $( '#site-navigation' ).on( 'click', '.menu-toggle', function(e) {
+    e.preventDefault();
 
+    $( 'body' ).toggleClass( 'active-secondary' );
+  } );
+
+  $( '#secondary' ).on( 'click', '.sidebar-close', function(e) {
+    $( 'body' ).removeClass( 'active-secondary' );
+  } );
 });
